b592aa1690b1e3b4158bac669963c5c95135ae6f
anthony_f <afanous@hotmail.com>
2023-06-29 10:31:18 -0400

    Exercise complete

--- js/script.js
+++ js/script.js
@@ -1,2 +1,25 @@
 const topicElement = document.querySelector(".topic");
 const countElement = document.querySelector(".count");
+
+const learning = {
+    topic : "JS",
+    learningGoals : ["React Skills", "Build JS Programs", "Learning threeJS", "Building Interactive Experiences"],
+    category : "Front End Development",
+    topicImportance : "high"
+}
+
+// console.log(learning);
+
+learning.topic = "JavaScript";
+
+learning.learningGoals.splice(1,1);
+
+// console.log(learning)
+
+topicElement.innerText = `✔️ I'm learning ${learning.topic}.`;
+
+topicElement.classList.remove("hide");
+
+countElement.innerText = `✔️ I have ${learning.learningGoals.length} learning goals`;
+
+countElement.classList.remove("hide");
